f609945a388e20d5c0bed2111b6f509d68153e24
pwener <phwener@gmail.com>
2017-05-18 00:21:16 +0000

    First version of calculator

--- django/NoConMaMu/matriz_calculator.py
+++ django/NoConMaMu/matriz_calculator.py
@@ -1,1 +1,29 @@
 # Code calculator here
+
+# Sample 3x3
+X = [[12, 7, 3],
+	[4, 5, 6],
+	[7, 8, 9]]
+
+
+# Sample 3x4
+Y = [[5, 8, 1, 2],
+	[6, 7, 3, 0],
+	[4, 5, 9, 1]]
+
+# Result is 3x4
+R = [[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[0, 0, 0, 0]]
+
+
+for i in range(len(X)):
+	# New matriz have same quantity of Y rows
+	for j in range(len(Y[0])):
+		for k in range(len(Y)):
+			R[i][j] += X[i][k] * Y[k][j]
+
+for r in R:
+	print(r)
+
+
